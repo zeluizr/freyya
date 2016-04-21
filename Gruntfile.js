d51812b886bf75d92675569d52ed6faa140f625f
Ze Luiz Rodrigues <zeluizr@thefirm.com.br>
2016-04-21 13:03:14 -0300

    bugfix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,20 @@
 module.exports = function(grunt) {
-
 	var src = grunt.option('src');
 	var dest = grunt.option('dest');
-
-	src = (dest == null) ? "src/" : src;
-	dest = (dest == null) ? "dist/" : dest;
-
+	if (src == null) {
+		src = "src/"
+	}
+	if (dest == null) {
+		dest = "dist/"
+	}
 	grunt.initConfig({
 		imagemin: {
 			main: {
 				files: [{
 					expand: true,
-					cwd: 'src/',
+					cwd: src,
 					src: ['**/*.{png,jpg,gif}'],
-					dest: 'dist/'
+					dest: dest
 				}]
 			}
 		}
